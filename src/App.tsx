3f26a17619f481d23e8169df00c35a97cb88fef2
hannupekka <hannupekka@gmail.com>
2020-09-06 12:42:11 +0300

    Move cron parsing to backend.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 import dayjs from 'dayjs';
 import Linkify from 'linkifyjs/react';
 import relativeTime from 'dayjs/plugin/relativeTime';
-import cronParser from 'cron-parser';
 import './App.css';
 
 dayjs.extend(relativeTime);
@@ -14,6 +13,7 @@ interface IChore {
   description?: string;
   schedule: string;
   done_at: string;
+  next_at: string;
 }
 
 const apiClient = axios.create({
@@ -54,105 +54,62 @@ const App = () => {
   };
 
   const renderChores = () => {
-    return chores
-      .sort((a: IChore, b: IChore) => {
-        const nextExecutionA = dayjs(
-          cronParser
-            .parseExpression(a.schedule, {
-              currentDate: dayjs(a.done_at)
-                .startOf('day')
-                .toDate(),
-            })
-            .next()
-            .toString()
-        )
-          .startOf('day')
-          .diff(new Date(), 'millisecond');
+    return chores.map((chore: IChore) => {
+      const { id, title, description, schedule, done_at, next_at } = chore;
+      const [, , day, month, weekday] = schedule.split(' ');
 
-        const nextExecutionB = dayjs(
-          cronParser
-            .parseExpression(b.schedule, {
-              currentDate: dayjs(b.done_at)
-                .startOf('day')
-                .toDate(),
-            })
-            .next()
-            .toString()
-        )
-          .startOf('day')
-          .diff(new Date(), 'millisecond');
+      const nextExecution = dayjs(next_at);
+      const isDue = dayjs().isSame(nextExecution, 'day');
+      const isLate = dayjs().isAfter(nextExecution, 'day');
 
-        return nextExecutionA < nextExecutionB ? -1 : 1;
-      })
-      .map((chore: IChore) => {
-        const { id, title, description, schedule, done_at } = chore;
-        const [, , day, month, weekday] = schedule.split(' ');
+      const choreClasses = ['chore', ...[!isDue && 'due'], ...[isLate && 'late']].filter(Boolean);
 
-        const lastExecution = dayjs(done_at)
-          .startOf('day')
-          .toDate();
-
-        // Calculate next execution from the last execution.
-        const parsed = cronParser.parseExpression(schedule, { currentDate: lastExecution });
-        const nextScheduledExecution = dayjs(parsed.next().toString()).startOf('day');
-
-        // Check if chore has alreade been done today.
-        const alreadyDoneToday = dayjs(lastExecution).isSame(nextScheduledExecution, 'day');
-        const nextExecution = alreadyDoneToday
-          ? dayjs(parsed.next().toString()).startOf('day')
-          : nextScheduledExecution;
-
-        const isDue = dayjs().isSame(nextExecution, 'day');
-        const isLate = dayjs().isAfter(nextExecution, 'day');
-
-        const choreClasses = ['chore', ...[!isDue && 'due'], ...[isLate && 'late']].filter(Boolean);
-
-        return (
-          <div key={id} className={choreClasses.join(' ')}>
-            <div className="title">
-              <div className="title--left">{title}</div>
-              <div className="title--right">
-                {(isDue || isLate) && (
-                  <button
-                    onClick={() => handleChore(id)}
-                    className={`button button--${isDue ? 'due' : 'late'}`}
-                  >
-                    {isDue ? 'Today' : 'Late'}
-                  </button>
-                )}
-              </div>
-            </div>
-            {description && (
-              <div className="description">
-                <Linkify>{description}</Linkify>
-              </div>
-            )}
-            <div className="schedule">
-              <div className="schedule--left">
-                <span className="crontab">
-                  <code className="prefix">Day</code>
-                  <code>{day}</code>
-                </span>
-                <span className="crontab">
-                  <code className="prefix">Month</code>
-                  <code>{month}</code>
-                </span>
-                <span className="crontab">
-                  <code className="prefix">Weekday</code>
-                  <code>{weekday}</code>
-                </span>
-              </div>
-              <div className="schedule--right">
-                Last done{' '}
-                {dayjs(done_at)
-                  .startOf('day')
-                  .format('D.M.YYYY')}{' '}
-                and {isLate ? 'was' : 'is'} due {formatNextExecution(nextExecution)}
-              </div>
+      return (
+        <div key={id} className={choreClasses.join(' ')}>
+          <div className="title">
+            <div className="title--left">{title}</div>
+            <div className="title--right">
+              {(isDue || isLate) && (
+                <button
+                  onClick={() => handleChore(id)}
+                  className={`button button--${isDue ? 'due' : 'late'}`}
+                >
+                  {isDue ? 'Today' : 'Late'}
+                </button>
+              )}
             </div>
           </div>
-        );
-      });
+          {description && (
+            <div className="description">
+              <Linkify>{description}</Linkify>
+            </div>
+          )}
+          <div className="schedule">
+            <div className="schedule--left">
+              <span className="crontab">
+                <code className="prefix">Day</code>
+                <code>{day}</code>
+              </span>
+              <span className="crontab">
+                <code className="prefix">Month</code>
+                <code>{month}</code>
+              </span>
+              <span className="crontab">
+                <code className="prefix">Weekday</code>
+                <code>{weekday}</code>
+              </span>
+            </div>
+            <div className="schedule--right">
+              Last done{' '}
+              {dayjs(done_at)
+                .startOf('day')
+                .format('D.M.YYYY')}{' '}
+              and {isLate ? 'was' : 'is'} due {formatNextExecution(nextExecution)}
+            </div>
+          </div>
+        </div>
+      );
+    });
   };
 
   return isLoading ? (
